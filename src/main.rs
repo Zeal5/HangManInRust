396a1f2017d473c277652585e8dbb6737d65450d
ZEAL <tassadiqshakeel5@gmail.com>
2023-03-22 20:13:19 +0500

    MVP completed

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,27 @@ fn main() {
     }
     // lives
     let mut lives = 7;
-    let user_char: char = read_user_input().to_ascii_lowercase();
+
+    while lives > 0 && (secret_word != guessed_word) {
+        println!("Guess a letter");
+        let user_char: char = read_user_input().to_ascii_lowercase();
+
+        if secret_word.contains(user_char) {
+            lives -= 1;
+            let mut v: Vec<usize> = vec![];
+            for (i, c) in secret_word.char_indices() {
+                if c == user_char {
+                    v.push(i)
+                }
+            }
+            for index in v {
+                guessed_word.replace_range(index..index + 1, user_char.to_string().as_str())
+            }
+        } else {
+            lives -= 1
+        }
+        println!("{}", guessed_word);
+    }
 }
 
 fn read_user_input() -> char {
