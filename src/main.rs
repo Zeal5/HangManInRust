ec95ab1cca82f12f3d2b091339a7f2798f098595
ZEAL <tassadiqshakeel5@gmail.com>
2023-03-22 20:11:48 +0500

    get user input

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,42 @@
+// HANGMAN GAME IN RUST
+use std::io;
 
+fn main() {
+    // secert word
+    let secret_word: String = "alian".to_ascii_lowercase();
+
+    // prep guess word
+    let mut guessed_word: String = String::from("");
+    for _ in 0..secret_word.len() {
+        guessed_word.push('_');
+    }
+    let user_char: char = read_user_input().to_ascii_lowercase();
+
+
+
+
+}
+
+
+
+
+
+
+
+
+fn read_user_input() -> char {
+    let mut user_input = String::new();
+    match io::stdin().read_line(&mut user_input) {
+        Ok(_) => match user_input.chars().next() {
+            Some(c) => return c,
+            None => {
+                println!("please input a char");
+                return '*';
+            }
+        },
+        Err(e) => {
+            println!("{e}");
+            return '*';
+        }
+    }
+}
